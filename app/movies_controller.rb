7b94647261a3ee5e469eb224fee55dc1d4bae179
Scott Wilson <scottwilson1020@gmail.com>
2020-09-04 17:34:03 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -76,8 +76,9 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  __
-  __
+   movie = Movie.find_by(title: "Awesome Flick")
+   movie.update(title: "Even Awesomer Flick")
+  __	  movie.save 
 end
 
 def can_update_multiple_items_at_once
